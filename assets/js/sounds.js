9caec6585b9178eb49e52e3283b75e4ac777c180
Alif Fadhillah <117177268+AlifFadhillahh@users.noreply.github.com>
2024-07-06 14:00:32 +0800

    Delete assets directory

--- assets/js/sounds.js
+++
@@ -1,9 +0,0 @@
-export const sounds = {
-    marin: new Audio('../assets/audio/marin.mp3'),
-    kobo: new Audio('../assets/audio/kobo.mp3'),
-    gura: new Audio('../assets/audio/gura.mp3'),
-    lily: new Audio('../assets/audio/lily.mp3'),
-    ninomae: new Audio('../assets/audio/ninomae.mp3'),
-    sfx_celebrate_horn: new Audio('../assets/audio/sfx_celebrate_horn.mp3'),
-    sfx_windows_error: new Audio('../assets/audio/sfx_windows_error.mp3'),
-};
